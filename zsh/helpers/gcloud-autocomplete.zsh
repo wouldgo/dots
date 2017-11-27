98acaa5e3af0dee9e0af6298745a447dd8b75100
wouldgo <wouldgo84@gmail.com>
2017-11-27 14:06:09 +0100

    gcloud conf

---
+++ zsh/helpers/gcloud-autocomplete.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/dario/google-cloud-sdk/path.zsh.inc' ]; then source '/home/dario/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/home/dario/google-cloud-sdk/completion.zsh.inc' ]; then source '/home/dario/google-cloud-sdk/completion.zsh.inc'; fi
