02da11803e01f48b5c1fd7ccae68ef7c0f696ae1
wouldgo <wouldgo84@gmail.com>
2019-04-29 15:46:21 +0200

    virtualenv

--- zsh/helpers/load-virtualenv.zsh
+++ zsh/helpers/load-virtualenv.zsh
@@ -2,13 +2,18 @@
 
 load-virtualenv() {
   if [[ -f ".venv" ]]; then
-    _VENV_PATH=$VENV_HOME/$(cat .venv)
+    _VENV_NAME=$(cat .venv)
+    _VENV_PATH="${VENV_HOME}/${_VENV_NAME}"
+
+    if [[ ! -d "${_VENV_PATH}" ]]; then
+      python3 -m venv ${_VENV_PATH}
+    fi
 
     # Check to see if already activated to avoid redundant activating
-    if [[ "$VIRTUAL_ENV" != $_VENV_PATH ]]; then
-      source $_VENV_PATH/bin/activate
+    if [[ "${VIRTUAL_ENV}" != "${_VENV_PATH}" ]]; then
+      source "${_VENV_PATH}"/bin/activate
     fi
-  elif [ -n "$VIRTUAL_ENV" ]; then
+  elif [ -n "${VIRTUAL_ENV}" ]; then
 
     deactivate
   fi
