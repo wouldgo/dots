f0faa65a23fa6913cfe5074511f1008a1bfb0271
wouldgo <wouldgo84@gmail.com>
2020-07-22 16:21:13 +0200

    android

--- zsh/helpers/android.zsh
+++ zsh/helpers/android.zsh
@@ -1,4 +1,11 @@
 #!/usr/bin/env zsh
 
-path=('/home/dario/android/platform-tools' '/home/dario/android/apktool' $path)
+export ANDROID_SDK_ROOT='/home/dario/android'
+export ANDROID_HOME=${ANDROID_SDK_ROOT}
+
+path=(
+  "${ANDROID_SDK_ROOT}/platform-tools"
+  "${ANDROID_SDK_ROOT}/apktool"
+  "${ANDROID_SDK_ROOT}/emulator"
+  $path)
 export PATH
