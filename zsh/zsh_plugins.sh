976eef2dc9332f3a54e1863c2bd8631142e7117e
wouldgo <wouldgo84@gmail.com>
2020-12-16 09:51:50 +0100

    missing some integrations

--- zsh/zsh_plugins.sh
+++ zsh/zsh_plugins.sh
@@ -4,7 +4,12 @@ source /home/dario/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-oh
 fpath+=( /home/dario/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/git )
 source /home/dario/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/nvm/nvm.plugin.zsh
 fpath+=( /home/dario/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/nvm )
+source /home/dario/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/node/node.plugin.zsh
+fpath+=( /home/dario/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/node )
+source /home/dario/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/npm/npm.plugin.zsh
+fpath+=( /home/dario/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/npm )
 fpath+=( /home/dario/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/docker )
+fpath+=( /home/dario/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/cargo )
 source /home/dario/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mttrs-SLASH-zsh-git-prompt/git-prompt.plugin.zsh
 fpath+=( /home/dario/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mttrs-SLASH-zsh-git-prompt )
 source /home/dario/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-dgnest-SLASH-zsh-gvm-plugin/zsh-gvm-plugin.zsh
