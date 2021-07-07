c6861a4150af1076551e601efea3b95281965546
wouldgo <wouldgo84@gmail.com>
2021-07-07 22:37:56 +0200

    refactored

--- zsh/confs/user.sh
+++ zsh/confs/user.sh
@@ -7,8 +7,9 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
   exit 1;
 ) \
 
-source "${CURRENT_DIR}/krew.sh"
-
 curl -sfL git.io/antibody | sh -s - -b "${CURRENT_DIR}/.." && \
 "${CURRENT_DIR}/../antibody-update-plugins.sh" && \
 ln -s "${CURRENT_DIR}/../.zshrc" ~/.zshrc
+
+source "${CURRENT_DIR}/krew.sh"
+source "${CURRENT_DIR}/../colors/index.sh"
