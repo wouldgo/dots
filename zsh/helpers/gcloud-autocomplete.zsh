154f67459f8472f1cb63aeb521ad1c47e4c4f58a
wouldgo <wouldgo84@gmail.com>
2022-05-09 11:50:31 +0200

    gke deprecation

--- zsh/helpers/gcloud-autocomplete.zsh
+++ zsh/helpers/gcloud-autocomplete.zsh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+export USE_GKE_GCLOUD_AUTH_PLUGIN="True"
+
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f "${HOME}/google-cloud-sdk/path.zsh.inc" ]; then
   source "${HOME}/google-cloud-sdk/path.zsh.inc";
