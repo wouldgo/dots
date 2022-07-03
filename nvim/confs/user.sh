fffcaea009d27db0b904ad21506a479d17fe3017
wouldgo <wouldgo84@gmail.com>
2022-07-03 21:45:39 +0200

    tmux with vim mode

---
+++ nvim/confs/user.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+NVIM_HOME="${HOME}/.config/nvim"
+
+rm -Rfv "${NVIM_HOME}"
+mkdir -p "${NVIM_HOME}"
+
+ln -s "${CURRENT_DIR}/../lua" "${NVIM_HOME}/lua"
+ln -s "${CURRENT_DIR}/../init.vim" "${NVIM_HOME}/init.vim"
