499f1ddeb4d5b58a8c8c690b5f41080242fe6fb5
wouldgo <wouldgo84@gmail.com>
2022-07-06 22:19:45 +0200

    no way

--- nvim/lua/wouldgo/configurations.lua
+++ nvim/lua/wouldgo/configurations.lua
@@ -1,24 +1,35 @@
 local options = {
+  guifont = 'monospace:h18',               -- the font used in graphical neovim applications
   updatetime = 50,
+  errorbells = false,
   nu = true,
   clipboard = 'unnamedplus',
   mouse = '',
   guicursor = '',
+  expandtab = true,
+  shiftwidth = 2,
+  tabstop = 2,
+  softtabstop = 2,
   colorcolumn = '100',
   cmdheight = 1,
   showmode = false,
-  --backup = false,                          -- creates a backup file
+  pumheight = 10,
+  conceallevel = 0,
+  fileencoding = 'utf-8',
+  undofile = true,                         -- enable persistent undo
+  undodir = vim.fn.stdpath('config') .. '/undodir',
+  writebackup = true,
+  backup = true,                          -- creates a backup file
+  backupdir = vim.fn.stdpath('config') .. '/backupdir',
+
   --completeopt = {                          -- mostly just for cmp
   --  'menuone',
   --  'noselect'
   --},
-  --conceallevel = 0,                        -- so that `` is visible in markdown files
-  --fileencoding = 'utf-8',                  -- the encoding written to a file
   --hidden = true,                           -- required to keep multiple buffers and open multiple buffers
   --hlsearch = false,                        -- no highlight all matches on previous search pattern
   --incsearch = true,
   --ignorecase = true,                       -- ignore case in search patterns
-  --pumheight = 10,                          -- pop up menu height
   --showtabline = 2,                         -- always show tabs
   --smartcase = true,                        -- smart case
   --smartindent = true,                      -- make indenting smarter again
@@ -27,11 +38,8 @@ local options = {
   --swapfile = false,                        -- creates a swapfile
   --termguicolors = true,                    -- set term gui colors (most terminals support this)
   --timeoutlen = 100,                        -- time to wait for a mapped sequence to complete (in milliseconds)
-  --writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-  --expandtab = true,                        -- convert tabs to spaces
-  --shiftwidth = 2,                          -- the number of spaces inserted for each indentation
-  --tabstop = 2,                             -- insert 2 spaces for a tab
-  --softtabstop = 2,
+
+
   --cursorline = true,                       -- highlight the current line
   --number = true,                           -- set numbered lines
   --relativenumber = false,                  -- set relative numbered lines
@@ -40,10 +48,9 @@ local options = {
   --wrap = false,                            -- display lines as one long line
   --scrolloff = 8,                           -- is one of my fav
   --sidescrolloff = 8,
-  --guifont = 'monospace:h17',               -- the font used in graphical neovim applications
-  --errorbells = false,
-  --undodir = vim.fn.stdpath('config') .. '/undodir',
-  --undofile = true,                         -- enable persistent undo
+
+
+
 }
 
 -- Don't pass messages to |ins-completion-menu|.
