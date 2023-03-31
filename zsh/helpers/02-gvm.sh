ed3325c84f99befc88c2298bca37ccb6deca416c
wouldgo <wouldgo84@gmail.com>
2023-03-31 15:21:13 +0200

    missconfig

--- zsh/helpers/02-gvm.sh
+++ zsh/helpers/02-gvm.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+if [ -s "$HOME/.gvm/scripts/gvm" ]; then
+
+  # shellcheck source=/dev/null
+  source "${HOME}/.gvm/scripts/gvm"
+fi
+
 if [ -f "${HOME}/.gvm/scripts/completion" ]; then
-  export GVM_ROOT="${HOME}/.gvm"
 
   # shellcheck source=/dev/null
   source "${HOME}/.gvm/scripts/completion"
