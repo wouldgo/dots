61414bf3464f4e62663ac94c19282702cc6e7d70
wouldgo <wouldgo84@gmail.com>
2023-10-25 20:37:21 +0200

    messy output

--- zsh/helpers/99-angular.zsh
+++ zsh/helpers/99-angular.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function __ng_bootstrap () {
-  if [ "$(rtx which ng)" ]; then
+  if [ "$(rtx which ng > /dev/null 2>&1)" ]; then
     local NG_COMMAND=$(rtx which ng)
 
     source <(${NG_COMMAND} completion script)
