6c49f6e0f68457516bff57ba5a504e1ef47abfa0
wouldgo <wouldgo84@gmail.com>
2024-08-25 11:37:43 +0200

    cleanse

--- alacritty/index.sh
+++ alacritty/index.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+function do_it () {
+  local CURRENT_DIR
+  CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
-[[ ! -f "${HOME}/.alacritty.toml" ]] || (
-  echo "alacritty configurations are already done" 2>&1
-  exit 1;
-) \
+  (cd "${CURRENT_DIR}/" || exit; curl -LOk https://raw.githubusercontent.com/alacritty/alacritty-theme/master/themes/gruvbox_dark.toml) && \
 
-(cd "${CURRENT_DIR}/../" || exit; curl -LOk https://raw.githubusercontent.com/alacritty/alacritty-theme/master/themes/gruvbox_dark.toml) && \
+  ln -s "${CURRENT_DIR}/gruvbox_dark.toml" "${HOME}/.gruvbox_dark.toml"
+  ln -s "${CURRENT_DIR}/alacritty.toml" "${HOME}/.alacritty.toml"
+}
 
-ln -s "${CURRENT_DIR}/../gruvbox_dark.toml" "${HOME}/.gruvbox_dark.toml"
-ln -s "${CURRENT_DIR}/../alacritty.toml" "${HOME}/.alacritty.toml"
+do_it "$@"
--- zsh/helpers/00-zinit.zsh
+++ zsh/helpers/00-zinit.zsh
@@ -4,7 +4,7 @@ ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 
 function __zinit_bootstrap () {
   if [ ! -d "$ZINIT_HOME" ]; then
-    mkdir -p "$(dirname $ZINIT_HOME)"
+    mkdir --parents "$(dirname $ZINIT_HOME)"
     git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
   fi
 
