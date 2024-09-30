fef499b52c56df051eb8b37ae957243bcd8987da
wouldgo <wouldgo84@gmail.com>
2024-09-30 13:27:49 +0200

    wsl config :(

---
+++ wsl/index.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+function do_it () {
+  local CURRENT_DIR
+
+  CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+
+  cat "-- ORIGINAL --"
+  printf "\r\n"
+  cat "/etc/wsl.conf"
+  printf "\r\n"
+  cat "-- OWN --"
+  printf "\r\n"
+  cat "${CURRENT_DIR}/wsl.conf"
+
+  read -rp "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
+  ln -sf "${CURRENT_DIR}/wsl.conf" "/etc/wsl.conf"
+}
+
+do_it "$@"
