4a54109452785e519dc283034459e43a2291e5bd
wouldgo <wouldgo84@gmail.com>
2025-02-06 21:44:08 +0100

    issue on color in alacritty

--- zsh/colors/index.sh
+++ zsh/colors/index.sh
@@ -5,6 +5,18 @@ function do_it () {
   CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
   curl -o "${CURRENT_DIR}/gruvbox.dircolors" https://raw.githubusercontent.com/twisty/dotfiles/master/gruvbox.dircolors
+  sed -i '/^TERM alacritty$/d' "${CURRENT_DIR}/gruvbox.dircolors"
+  awk '
+    /^TERM / { last = NR }  # Track the last line number where TERM appears
+    { lines[NR] = $0 }      # Store all lines
+    END {
+      for (i = 1; i <= NR; i++) {
+        print lines[i]
+        if (i == last) print "TERM alacritty"  # Insert TERM alacritty after last TERM line
+      }
+    }
+  ' "${CURRENT_DIR}/gruvbox.dircolors" > "${CURRENT_DIR}/gruvbox.dircolors.temp" && \
+  mv "${CURRENT_DIR}/gruvbox.dircolors.temp" "${CURRENT_DIR}/gruvbox.dircolors"
 }
 
 do_it "$@"
