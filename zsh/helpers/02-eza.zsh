b36919a77156ee3ab776f75adc660e6931cef108
wouldgo <wouldgo84@gmail.com>
2026-07-19 00:16:31 +0200

    eza show groups

--- zsh/helpers/02-eza.zsh
+++ zsh/helpers/02-eza.zsh
@@ -4,7 +4,7 @@ function __eza_bootstrap () {
   if [ "$(mise which -q eza 2> /dev/null)" ]; then
     local EZA_BIN=$(mise which eza)
 
-    alias ls="$EZA_BIN"
+    alias ls="$EZA_BIN --group"
   fi
 }
 
