b5dd636cfd047bd1425b485fb3fcd94fc5d65cf5
wouldgo <1098882+wouldgo@users.noreply.github.com>
2026-09-16 22:31:50 +0200

    krew autocomplete is no more there

--- zsh/helpers/03-kubectl-krew.zsh
+++ zsh/helpers/03-kubectl-krew.zsh
@@ -1,10 +1,23 @@
 #!/usr/bin/env zsh
 
 function __kubectl_krew_boostrap () {
-  if [ "$(mise which -q krew 2> /dev/null)" ] && [ ! -f "${ZSH_COMPLETION_FOLDER}/_kubectl-krew.zsh" ]; then
-    local KREW_BIN=$(mise which helm)
+  if [ "$(mise which -q krew 2> /dev/null)" ]; then
+    local KREW_BIN=$(mise which krew)
 
-    "${KREW_BIN}" completion zsh | tee "${ZSH_COMPLETION_FOLDER}/_kubectl-krew.zsh" >/dev/null
+    path=(
+      "${KREW_ROOT:-$HOME/.krew}/bin"
+      $path)
+    export PATH
+
+    local KREW_PLUGIN_LIST=$("${KREW_BIN}" list 2> /dev/null)
+
+    local VIEW_ALLOCATIONS_INSTALLED=$(echo "${KREW_PLUGIN_LIST}" | grep 'view-allocations' 2> /dev/null)
+    if [[ "${VIEW_ALLOCATIONS_INSTALLED}" != "view-allocations" ]]; then
+      "${KREW_BIN}" install krew
+
+      "${KREW_BIN}" update
+      "${KREW_BIN}" install view-allocations
+    fi
   fi
 }
 
